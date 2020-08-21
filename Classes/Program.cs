ad88449d4b3e8a3aad58a59e1ff403afce552a1f
Nicholas A Perry <nicholasperrypro@gmail.com>
2020-08-20 20:03:38 -0400

    Added files

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,17 +10,37 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("What's yo name?");
+            string myName = Console.ReadLine();
+            Person randomPerson = new Person(myName);
+            randomPerson.SayMyName();
+            Console.ReadLine();
         }
     }
     // Class Declaration
     // To create a specific person, you create an instance of the class
     class Person
     {
+        readonly string _myName;
+        // Getters and Setters TBD
+
+        // public name-of-the-class
+        public Person(string name)
+        {
+            _myName = name;
+        }
         // Method declaration
-        void SayHello()
+        // access-modifier return-type name (parameters) 
+        // (argument1, argument2)
+        
+        public void SayHello()
         {
             Console.WriteLine("Hello");
+        }
+
+        public void SayMyName()
+        {
+            Console.WriteLine(_myName.ToLower());
         }
     }
 }
